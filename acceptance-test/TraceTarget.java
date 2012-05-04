2dc437b8ff59c20f4d90948b0798b0cb70df9074
jushi <jushi@taobao.com>
2012-05-04 19:52:44 +0800

    Test class loader.

--- acceptance-test/TraceTarget.java
+++ acceptance-test/TraceTarget.java
@@ -1,18 +1,90 @@
+/*
+ * Copyright 2012 zhongl
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.lang.*;
+import java.lang.Class;
+import java.lang.ClassLoader;
+import java.lang.ClassNotFoundException;
 import java.lang.Exception;
+import java.lang.IllegalAccessException;
+import java.lang.InstantiationException;
+import java.lang.NoSuchMethodException;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.System;
 import java.lang.Thread;
+import java.lang.reflect.InvocationTargetException;
 
 public class TraceTarget {
 
-    public static void main(String[] args) {
-        while (true){
+    public static void main(String[] args) throws Exception {
+        loadClass();
+
+        while (true) {
             addOne(0);
-            try{
+            try {
                 Thread.sleep(500L);
-            }catch (Exception e){
+            } catch (Exception e) {
                 break;
             }
         }
     }
 
-    public static int addOne(int i){ return i + 1; }
+    private static void loadClass() throws Exception {
+        Class<?> aClass = new CL().loadClass("TraceTarget$A");
+        Object o = aClass.getConstructor(String.class).newInstance("123");
+        System.out.println(o);
+    }
+
+    public static int addOne(int i) {
+        return i + 1;
+    }
+
+    public static class CL extends ClassLoader {
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            if (name.startsWith("Trace")) {
+                try {
+                    InputStream inputStream = getResourceAsStream(name + ".class");
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    int read = 0;
+                    while ((read = inputStream.read()) > -1) outputStream.write(read);
+                    byte[] bytes = outputStream.toByteArray();
+                    return defineClass(name, bytes, 0, bytes.length);
+                } catch (Exception e) {
+                    throw new ClassNotFoundException(name, e);
+                }
+            } else {
+                return getParent().loadClass(name);
+            }
+        }
+    }
+
+    public static class A {
+        public final String s;
+
+        public A(String s) {
+            this.s = s;
+            new B();
+        }
+    }
+
+    public static class B {}
 }

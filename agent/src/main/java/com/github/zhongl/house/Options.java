70e4b00c1e7c5d31f6b8003ffec398844a060641
zhongl <zhong.lunfu@gmail.com>
2012-05-05 21:04:20 +0800

    Clean code and update copyright.

--- agent/src/main/java/com/github/zhongl/house/Options.java
+++ agent/src/main/java/com/github/zhongl/house/Options.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 zhongl
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.github.zhongl.house;
 
 import java.io.File;
@@ -12,17 +28,19 @@ import java.util.Set;
  * @author: <a href = "mailto:zhong.lunfu@gmail.com">zhongl</a>
  */
 class Options {
-    private final Map<String, String> map = new HashMap<String, String>();
+    private final Map<String, String> map;
 
-    private Options(String[] options) {
-        for (String option : options) {
-            String[] pair = option.split("=");
-            map.put(pair[0], pair[1]);
-        }
+    private Options(Map<String, String> map) {
+        this.map = map;
     }
 
     public static Options parse(String arguments) {
-        return new Options(arguments.split("\\s+"));
+        final Map<String, String> map = new HashMap<String, String>();
+        for (String option : arguments.split("\\s+")) {
+            String[] pair = option.split("=");
+            map.put(pair[0], pair[1]);
+        }
+        return new Options(map);
     }
 
     public URL[] classLoaderUrls() {

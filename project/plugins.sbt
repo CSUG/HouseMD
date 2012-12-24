0d88ab52e691c27cc0ceba45c21a0bba3388791c
zhongl <zhong.lunfu@gmail.com>
2012-12-24 09:52:54 +0800

    Deprecate plugins.sbt.

--- project/plugins.sbt
+++
@@ -1,5 +0,0 @@
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0")
-
-resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-
-libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-proguard-plugin" % (v+"-0.1.1"))
--- project/project/Build.scala
+++ project/project/Build.scala
@@ -1,8 +1,17 @@
 import sbt._
 
-object PluginDef extends Build {
+object Build extends sbt.Build {
+
   override def projects = Seq(root)
 
-  lazy val root     = Project("plugins", file(".")) dependsOn (proguard)
   lazy val proguard = uri("git://github.com/senia-psm/xsbt-proguard-plugin.git")
+  lazy val root     = Project(
+    id = "plugins",
+    base = file("."),
+    settings = Defaults.defaultSettings ++ Seq(
+      addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0")
+    )
+  ) dependsOn (proguard)
+
+
 }

ade59a41ff8bc5ec7c3493dfb94ae7f371671f0e
zhongl <zhong.lunfu@gmail.com>
2013-06-24 19:41:06 +0800

    Refactor Global.

--- acceptance-test/Global.java
+++ acceptance-test/Global.java
@@ -1,61 +1,49 @@
-
 import sun.reflect.Reflection;
 
-import java.lang.*;
-import java.lang.Exception;
-import java.lang.Object;
-import java.lang.StackTraceElement;
-import java.lang.String;
-import java.lang.System;
-import java.lang.Thread;
-import java.lang.Throwable;
-import java.lang.ref.WeakReference;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Global {
+    // Avoiding overflow by blocking queue
     public static final BlockingQueue<Object> QUEUE = new ArrayBlockingQueue<Object>(1000);
-    public static final String                ENTER = ">";
-    public static final String                LEAVE = "<";
 
-    private static final Thread THEAD  = Thread.currentThread(); // HouseMD should load this class first.
-    private static final int    NOW    = 0x01;
-    private static final int    ARGS   = 0x02;
-    private static final int    RESULT = 0x04;
-    private static final int    STACK  = 0x08;
+    // HouseMD agent should load this class first
+    private static final Thread AGENT_THREAD = Thread.currentThread();
 
-    /**
-     * @param klass   class name
-     * @param method  method name
-     * @param self    this object, it would be null if method was static
-     * @param loader  class loader, it would be null if class was loaded by BootClassLoader
-     * @param args    method arguments
-     * @param options {@link #NOW} {@link #ARGS}
-     */
-    public static void enter(String klass, String method, Object self, Object loader, Object[] args, int options) {
+    // options enums
+    private static final int ELAPSE    = 0x01;
+    private static final int ARGS      = 0x02;
+    private static final int RESULT    = 0x04;
+    private static final int STACK     = 0x08;
+    private static final int EXCEPTION = 0x10;
+    private static final int THREAD    = 0x20;
+
+
+    public static void offer(String klass,
+                             String method,
+                             String descriptor,
+                             Object self,
+                             ClassLoader loader,
+                             int options,
+                             Object[] args,
+                             Object result,
+                             long elapse) {
         final Thread current = Thread.currentThread();
-        if (THREAD == current || QUEUE == self || Global.class == Reflection.getCallerClass(3)) return;
+        if (AGENT_THREAD == current || QUEUE == self || Global.class == Reflection.getCallerClass(3)) return;
 
-        final Object[] arguments = (options & ARGS) == ARGS ? args : null;
-        final Long now = (options & NOW) == NOW ? System.nanoTime() : 0L;
-
-        QUEUE.put(new Object[]{ENTER, current, klass, method, self, loader, arguments, now})
-    }
-
-    /**
-     * @param normal  false leave with an exception thrown.
-     * @param result  reture value or exception
-     * @param options {@link #NOW} {@link #RESULT} {@link #STACK}
-     */
-    public static void leave(boolean normal, Object result, int options) {
-        final Thread current = Thread.currentThread();
-        if (THREAD == current || QUEUE == self || Global.class == Reflection.getCallerClass(3)) return;
-
-        result = (options & RESULT) == RESULT ? result : null;
-        final Long now = (options & NOW) == NOW ? System.nanoTime() : 0L;
-        final StackTraceElement[] stack = (options & STACK) == STACK ? currentStackTrace() : null;
-
-        QUEUE.put(new Object[]{LEAVE, current, normal, result, now, stack})
+        QUEUE.offer(new Object[]{
+                klass,
+                method,
+                descriptor,
+                self,
+                loader,
+                (options & EXCEPTION) == EXCEPTION,
+                (options & ARGS) == ARGS ? args : null,
+                (options & RESULT) == RESULT ? result : null,
+                (options & ELAPSE) == ELAPSE ? elapse : -1L,
+                (options & THREAD) == THREAD ? current : null,
+                (options & STACK) == STACK ? currentStackTrace() : null
+        })
     }
 
     /**
--- acceptance-test/MethodInterceptor.java
+++ acceptance-test/MethodInterceptor.java
@@ -1,26 +1,42 @@
-import sun.reflect.Reflection;
-
-import java.lang.ClassLoader;
-import java.lang.Exception;
-import java.lang.Object;
-import java.lang.Throwable;
-
+import java.lang.System;
 
 public class MethodInterceptor {
+
+    final int options = Global.RESULT | Global.ELAPSE | Global.STACK | Global.THREAD | Global.ARGS;
 
     public Object intercept(Object obj, java.lang.reflect.Method method, Object[] args,
                             MethodProxy proxy) throws Throwable {
 
+        final Object self = obj;
         final ClassLoader loader = MethodInterceptor.class.getClassLoader();
 
-        Global.enter("class", "method", obj, loader, args, Global.ARGS || Global.NOW)
-
         try {
-            Object result = method.invoke(obj, args);
-            Global.leave(true, result, Global.RESULT || Global.NOW || Global.STACK)
+            long begin = System.nanoTime();
+            Object result = method.invoke(self, args);
+
+            Global.offset("class",
+                          "method",
+                          "descriptor",
+                          self,
+                          loader,
+                          options,
+                          args,
+                          result,
+                          System.nanoTime() - begin);
+
+            Global.leave(self, true, result, )
             return reuslt;
         } catch (Throwable t) {
-            Global.leave(true, t, Global.RESULT || Global.NOW || Global.STACK)
+
+            Global.offset("class",
+                          "method",
+                          "descriptor",
+                          self,
+                          loader,
+                          options | Global.EXCEPTION,
+                          args,
+                          t,
+                          System.nanoTime() - begin);
             throw t;
         }
 
